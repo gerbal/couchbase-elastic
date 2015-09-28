2dc9463f0a1463881f5265901c9ab29d3cd305bd
clakech <cyril.lakech@adeoservices.com>
2015-09-28 22:56:02 +0200

    config couchbase + elastic clustering

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ COPY init.sh /
 
 ENTRYPOINT ["/init.sh"]
 
+EXPOSE 8091
 
--- init.sh
+++ init.sh
@@ -13,22 +13,28 @@ wait_for_start() {
 echo "launch couchbase"
 /entrypoint.sh couchbase-server &
 
-wait_for_start couchbase-cli cluster-init -c 127.0.0.1:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --cluster-init-username=${ADMIN_LOGIN} --cluster-init-password=${ADMIN_PASSWORD} --cluster-init-port=8091 --cluster-init-ramsize=${CLUSTER_RAM_QUOTA}
+if [ "$INIT_CLUSTER" = "1" ]; then
+    wait_for_start couchbase-cli cluster-init -c 127.0.0.1:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --cluster-init-username=${ADMIN_LOGIN} --cluster-init-password=${ADMIN_PASSWORD} --cluster-init-port=8091 --cluster-init-ramsize=${CLUSTER_RAM_QUOTA}
 
-wait_for_start couchbase-cli bucket-create --cluster=127.0.0.1 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --bucket=data --bucket-type=couchbase --bucket-ramsize=512 --wait
+    couchbase-cli bucket-create --cluster=127.0.0.1 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --bucket=data --bucket-type=couchbase --bucket-ramsize=512 --wait
 
-wait_for_start couchbase-cli setting-xdcr -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --max-concurrent-reps=8
-wait_for_start couchbase-cli xdcr-setup -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD \
-    --create \
-    --xdcr-cluster-name=ElasticCouchbase \
-    --xdcr-hostname=elastic-couchbase:9091 \
-    --xdcr-username=$ADMIN_LOGIN \
-    --xdcr-password=$ADMIN_PASSWORD
-wait_for_start couchbase-cli xdcr-replicate -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD \
-    --xdcr-cluster-name=ElasticCouchbase \
-    --xdcr-from-bucket=data \
-    --xdcr-to-bucket=data \
-	--xdcr-replication-mode=capi
+    couchbase-cli setting-xdcr -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --max-concurrent-reps=8
+    couchbase-cli xdcr-setup -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD \
+        --create \
+        --xdcr-cluster-name=ElasticCouchbase \
+        --xdcr-hostname=elastic-couchbase:9091 \
+        --xdcr-username=$ADMIN_LOGIN \
+        --xdcr-password=$ADMIN_PASSWORD
+    couchbase-cli xdcr-replicate -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD \
+        --xdcr-cluster-name=ElasticCouchbase \
+        --xdcr-from-bucket=data \
+        --xdcr-to-bucket=data \
+        --xdcr-replication-mode=capi
+else
+    ip=`hostname --ip-address`
+    wait_for_start couchbase-cli rebalance -c couchbase-first:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --server-add=$ip:8091 --server-add-username=$ADMIN_LOGIN --server-add-password=$ADMIN_PASSWORD
+fi
+
 wait
 
 
